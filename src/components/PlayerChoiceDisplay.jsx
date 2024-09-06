02e8ab618a0f686e3a54c8275eda4ef6887f229d
rebs-code <jack.rebussi@gmail.com>
2024-09-06 15:33:15 +0200

    fix: capitalize player choices in PlayerChoiceDisplay

--- src/components/PlayerChoiceDisplay.jsx
+++ src/components/PlayerChoiceDisplay.jsx
@@ -21,13 +21,17 @@ export default function PlayerChoiceDisplay({
         <h2 className="text-xl font-bold mb-4 text-slate-200">
           {getPlayerName(1)}
         </h2>
-        <p className="text-4xl text-slate-100">{player1Choice || "?"}</p>
+        <p className="text-4xl text-slate-100 capitalize">
+          {player1Choice || "?"}
+        </p>
       </div>
       <div className="bg-slate-800 p-6 rounded-lg text-center w-full md:w-48">
         <h2 className="text-xl font-bold mb-4 text-slate-200">
           {getPlayerName(2)}
         </h2>
-        <p className="text-4xl text-slate-100">{player2Choice || "?"}</p>
+        <p className="text-4xl text-slate-100 capitalize">
+          {player2Choice || "?"}
+        </p>
       </div>
     </div>
   );
